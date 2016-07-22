e9370d462288b3a0521af4e831a24f3a9ab335bd
Jair Huaman <jairfrancesco@gmail.com>
2016-07-22 04:07:03 -0500

    Version Final Funcionando

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -10,6 +10,7 @@ class HomeController{
     }
     
     public function Index(){
+        header('Location:'.BASE_URL.'Cronogramas');
         require_once 'Views/header.php';
         require_once 'Views/sidebar.php';
         require_once 'Views/panel.php';
